99b2605541d4cd25de632944ca5f1b3d1d133321
OmarMeriwani <omar.sirwan@gmail.com>
2019-04-16 20:59:24 +0100

    Word2vec model

--- CreateEmbeddingModel.py
+++ CreateEmbeddingModel.py
@@ -1,5 +1,12 @@
 from nltk.corpus import stopwords
 import string
+from string import punctuation
+from os import listdir
+from collections import Counter
+from nltk.corpus import stopwords
+from keras.preprocessing.text import Tokenizer
+from keras.preprocessing.sequence import pad_sequences
+from gensim.models import Word2Vec
 
 def load_doc(filename):
 	# open the file as read only
@@ -10,3 +17,63 @@ def load_doc(filename):
 	file.close()
 	return text
 
+def doc_to_clean_lines(doc, vocab):
+	clean_lines = list()
+	lines = doc.splitlines()
+	for line in lines:
+		# split into tokens by white space
+		tokens = line.split()
+		# remove punctuation from each token
+		table = str.maketrans('', '', punctuation)
+		tokens = [w.translate(table) for w in tokens]
+		# filter out tokens not in vocab
+		tokens = [w for w in tokens if w in vocab]
+		clean_lines.append(tokens)
+	return clean_lines
+def process_docs2(directory, vocab, is_trian):
+	lines = list()
+	# walk through all files in the folder
+	for filename in listdir(directory):
+		# create the full path of the file to open
+		path = directory + '/' + filename
+		# load and clean the doc
+		doc = load_doc(path)
+		doc_lines = doc_to_clean_lines(doc, vocab)
+		# add lines to list
+		lines += doc_lines
+	return lines
+def process_docs2(directory, vocab, is_trian):
+	lines = list()
+	# walk through all files in the folder
+	for filename in listdir(directory):
+		# skip any reviews in the test set
+		if is_trian and filename.startswith('cv9'):
+			continue
+		if not is_trian and not filename.startswith('cv9'):
+			continue
+		# create the full path of the file to open
+		path = directory + '/' + filename
+		# load and clean the doc
+		doc = load_doc(path)
+		doc_lines = doc_to_clean_lines(doc, vocab)
+		# add lines to list
+		lines += doc_lines
+	return lines
+vocab_filename = 'D:/aclImdb/imdb.vocab'
+vocab = load_doc(vocab_filename)
+vocab = vocab.split()
+vocab = set(vocab)
+positive_docs = process_docs2('D:/aclImdb/train/pos', vocab, True)
+negative_docs = process_docs2('D:/aclImdb/train/neg', vocab, True)
+positive_docs2 = process_docs2('D:/aclImdb/test/pos', vocab, True)
+negative_docs2 = process_docs2('D:/aclImdb/test/neg', vocab, True)
+sentences = negative_docs + positive_docs + positive_docs2 + negative_docs2
+model = Word2Vec(sentences, size=100, window=5, workers=8, min_count=1)
+words = list(model.wv.vocab)
+filename = 'imdb_word2vec.txt'
+model.wv.save_word2vec_format(filename, binary=False)
+#tokenizer = Tokenizer()
+#tokenizer.fit_on_texts(train_docs)
+#encoded_docs = tokenizer.texts_to_sequences(train_docs)
+#max_length = max([len(s.split()) for s in train_docs])
+#Xtrain = pad_sequences(encoded_docs, maxlen=max_length, padding='post')
--- FeaturesExtraction.py
+++ FeaturesExtraction.py
@@ -5,3 +5,49 @@ df = pd.read_csv('train.csv',header=0,sep='\t')
 
 for i in range(0,len(df)):
     print(df.loc[i][2])
+
+'''
+Get stopwords
+lowercase
+Separate sentences
+POS
+Senti
+Negation fixing
+
+
+Get stopwords
+Get full sentence from each row with score
+Remove stop words from each line
+Convert to lower case
+Remove punctuation
+Get 1,2 words with scores
+Build dictionary of 1,2 words with score
+Sentence sentiments:
+    * Get max polarity for each sentence considering negation after removing stop words
+    * Create subj value according to the max polarity
+Store 
+-------------------
+After preparing the list:
+Tokenize
+Remove stop words
+Remove punctuation
+Re-do term frequency
+Divide terms/phrases into positive and negative
+Remove the intersecting list between the two lists
+Count the following:
+    * The number of negative/positive in the document
+    * The highest polarity of negative/positive words in the document
+    * Sum of max polarity of document sentences by applying the last step on sentence level
+-------------------
+Load word2vec
+Encode sentences
+
+Build CNN
+Get results
+
+Build RNN
+Get results
+
+Use the result as a feature with the best model from above to predict with the statistical model
+
+'''
